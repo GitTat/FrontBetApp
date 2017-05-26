bac4017322f8167211343e118b86d8d646a7f34f
GitTat <tkbegjanyan@gmail.com>
2017-05-26 16:47:30 +0400

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
-import Login from './Login.js'
-
-ReactDOM.render(<Login />, document.getElementById('root'));
-registerServiceWorker();
-
